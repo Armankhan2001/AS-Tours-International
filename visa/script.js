b5122e0178b2611abd0fd1529e34521b5e520298
Arman <armankhan232019@gmail.com>
2025-11-26 15:24:11 +0530

    added all the proper links & update Pages

--- visa/script.js
+++
@@ -1,9 +0,0 @@
-// visa-ServiceWorkerRegistration
-document.getElementById("countryDetails").addEventListener("click", function () {
-    window.location.href = "../t2-project/index.html";
-  });
-  
-  document.getElementById("visaCategories").addEventListener("click", function () {
-    window.location.href = "/visa.html";
-  });
-  
